4af628bb662dabe56c72b34f72363c2a6ce57789
takagotch <dyaccb@gmail.com>
2019-06-08 11:37:03 +0900

    Fabricate(:account, followers_url: '', uri: 'https://example.com/actor')

---
+++ fr/config/initializers/simple_form.rb
@@ -0,0 +1,45 @@
+
+module AppendComponent
+  def append(wrapper_options = nil)
+    @append ||= begin
+      options[:append].to_s.html_safe if options[:append].present?
+    end
+  end
+end
+
+module RecommendedComponent
+  def recommended(wrapper_options = nil)
+    return unless options[:recommended]
+    options[:label_text] = ->(raw_label_text, _required_label_text, _label_present) { safe_join([raw_label_text, ' ', content_tag(:span, content_tag(:span, I18n.t('simple_form.recommended'), class: 'recommended'))]) }
+    nil
+  end
+end
+
+SimpleForm.include_component(AppendComponent)
+SimpleForm.include_component(RecommendedComponent)
+
+SimpleForm.setup do |config|
+  config.wrappers :default, class: :input, hint_class: :field_with_hint, error_class: :field_with_errors do |b|
+    b.use :html5
+
+    b.use :placeholder
+
+    b.optional :maxlength
+
+    b.optional :pattern
+
+    b.optional :min_max
+
+    b.optional :readonly
+
+    b.use :input
+    b.use :hint, wrap_with: { tag: :span, class: :hint }
+    b.use :error, wrap_with: { tag: :span, class: :error }
+
+  end
+
+  config.wrappers :with_label, class: [], hint_class: :field_with_hint
+
+end
+
+
