1e23c848d8d36d2e8c60b00733959253b160f984
takagotch <dyaccb@gmail.com>
2019-06-08 10:01:21 +0900

    Fabricate(:account, followers_url: '', uri: 'https://example.com/actor')

---
+++ fr/spec/lib/activitypub/activity/announce_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe ActivityPub::Activity::Anounce do
+  let() {}
+  let() {}
+  let() {}
+
+  let() do
+  end
+
+  let() do
+  end
+
+  subject {}
+
+  describe '' do
+    context '' do
+    end
+
+    context '' do
+    end
+
+    it '' do
+    end
+  end
+
+  contect '' do
+  end
+
+  context '' do
+    let() do
+    end
+
+    it '' do
+    end
+  end
+end
+
+
